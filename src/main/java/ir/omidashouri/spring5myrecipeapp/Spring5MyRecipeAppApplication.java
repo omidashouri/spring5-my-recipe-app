cc572c420ba394a560fecb52ce210314f6e2cfde
Omid Ashouri <omidashouri@gmail.com>
2019-07-19 11:57:56 -0700

    recipe project start : omidashouri : 98.04.28

---
+++ src/main/java/ir/omidashouri/spring5myrecipeapp/Spring5MyRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.spring5myrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MyRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5MyRecipeAppApplication.class, args);
+    }
+
+}
